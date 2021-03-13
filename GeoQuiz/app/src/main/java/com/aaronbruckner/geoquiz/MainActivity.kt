8bb3bd32967e190cddf6206c6a490119c6879a5d
Aaron Bruckner <aaronbruckner@gmail.com>
2021-03-13 08:25:25 -0800

    Added True/False Buttons

--- GeoQuiz/app/src/main/java/com/aaronbruckner/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/aaronbruckner/geoquiz/MainActivity.kt
@@ -2,10 +2,34 @@ package com.aaronbruckner.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.StringRes
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            showResultToast(R.string.correct_toast)
+        }
+
+        falseButton.setOnClickListener {
+            showResultToast(R.string.incorrect_toast)
+        }
+    }
+
+    private fun showResultToast(@StringRes message: Int): Unit {
+        val toast: Toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 0)
+        toast.show()
     }
 }
