f36b0c2c0d253fe43cb4fe56f466765eeedccba4
Aaron Bruckner <aaronbruckner@gmail.com>
2021-03-14 08:41:50 -0700

    Added horizontal layout & updated quiz questions to remember user input.

--- GeoQuiz/app/src/main/java/com/aaronbruckner/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/aaronbruckner/geoquiz/MainActivity.kt
@@ -2,16 +2,21 @@ package com.aaronbruckner.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.StringRes
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: View
+    private lateinit var prevButton: View
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -26,12 +31,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate()")
         setContentView(R.layout.activity_main)
 
         // UI References
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         // On Click Handlers
@@ -41,18 +48,56 @@ class MainActivity : AppCompatActivity() {
         falseButton.setOnClickListener {
             checkAnswer(false)
         }
+        questionTextView.setOnClickListener {
+            moveToNextQuestion(1)
+        }
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            moveToNextQuestion(1)
+        }
+        prevButton.setOnClickListener {
+            moveToNextQuestion(-1)
         }
 
         // Init
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy()")
+    }
+
+    private fun moveToNextQuestion(direction: Int) {
+        currentIndex = (currentIndex + direction).coerceAtLeast(0) % questionBank.size
+        updateQuestion()
+    }
+
     private fun checkAnswer(userAnswer: Boolean) {
-        val isUserCorrect = getCurrentQuestion().answer == userAnswer
+        val question: Question = getCurrentQuestion()
+        question.userAnswer = userAnswer
+        val isUserCorrect = question.answer == userAnswer
         showResultToast(if (isUserCorrect) R.string.correct_toast else R.string.incorrect_toast)
+        updateQuestion()
     }
 
     private fun showResultToast(@StringRes message: Int): Unit {
@@ -62,7 +107,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
-        questionTextView.setText(getCurrentQuestion().textResId)
+        val question: Question = getCurrentQuestion()
+        questionTextView.setText(question.textResId)
+        trueButton.isEnabled = question.userAnswer == null
+        falseButton.isEnabled = question.userAnswer == null
     }
 
     private fun getCurrentQuestion(): Question {
