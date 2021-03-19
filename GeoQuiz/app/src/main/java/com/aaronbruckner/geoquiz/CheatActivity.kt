d8d9286b1fe446f5954429b42ab82ce759df27eb
Aaron Bruckner <aaronbruckner@gmail.com>
2021-03-18 19:28:43 -0700

    Added view model to cheat activity

--- GeoQuiz/app/src/main/java/com/aaronbruckner/geoquiz/CheatActivity.kt
+++ GeoQuiz/app/src/main/java/com/aaronbruckner/geoquiz/CheatActivity.kt
@@ -5,12 +5,25 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.viewModels
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import kotlinx.parcelize.Parcelize
 
 private const val EXTRA_IS_ANSWER_TRUE = "com.aaronbruckner.geoquiz.is_answer_true"
 private const val EXTRA_DID_CHEAT = "com.aaronbruckner.geoquiz.did_cheat"
+
+class CheatViewModel(private val state: SavedStateHandle) : ViewModel() {
+    var didCheat: Boolean = state.get(EXTRA_DID_CHEAT) ?: false
+        set(value) {
+            field = value
+            state.set(EXTRA_DID_CHEAT, value)
+        }
+}
 
 class CheatActivity : AppCompatActivity() {
     class DidCheatContract: ActivityResultContract<Boolean, Boolean>() {
@@ -31,6 +44,7 @@ class CheatActivity : AppCompatActivity() {
 
     private lateinit var showAnswerButton: View
     private lateinit var answerTextView: TextView
+    private val viewModel: CheatViewModel by viewModels()
 
     private val isAnswerTrue: Boolean by lazy {
         intent.getBooleanExtra(EXTRA_IS_ANSWER_TRUE, false)
@@ -46,15 +60,23 @@ class CheatActivity : AppCompatActivity() {
         showAnswerButton.setOnClickListener {
             showAnswer()
         }
+        if (viewModel.didCheat) {
+            showAnswer()
+        }
     }
 
     private fun showAnswer() {
-        setResult(Activity.RESULT_OK, Intent().apply {
-            putExtra(EXTRA_DID_CHEAT, true)
-        })
+        viewModel.didCheat = true
         answerTextView.setText(when {
             isAnswerTrue -> R.string.true_button
             else -> R.string.false_button
         })
+    }
+
+    override fun onBackPressed() {
+        setResult(Activity.RESULT_OK, Intent().apply {
+            putExtra(EXTRA_DID_CHEAT, true)
+        })
+        super.onBackPressed()
     }
 }
