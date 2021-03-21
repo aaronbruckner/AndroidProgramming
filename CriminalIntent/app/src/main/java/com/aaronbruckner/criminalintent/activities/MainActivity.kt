1b76b3b046aede012984f6bbabcf3c1e479129b7
Aaron Bruckner <aaronbruckner@gmail.com>
2021-03-21 09:40:08 -0700

    Initial Criminal Intent Commit

---
+++ CriminalIntent/app/src/main/java/com/aaronbruckner/criminalintent/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aaronbruckner.criminalintent.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aaronbruckner.criminalintent.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
