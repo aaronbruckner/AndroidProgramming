e8b15053f8083bcf558d81fc5a15b7326d5181c7
Aaron Bruckner <aaronbruckner@gmail.com>
2021-03-21 13:11:34 -0700

    Basic Crime Fragment created.

--- CriminalIntent/app/src/main/java/com/aaronbruckner/criminalintent/activities/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/aaronbruckner/criminalintent/activities/MainActivity.kt
@@ -3,10 +3,20 @@ package com.aaronbruckner.criminalintent.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aaronbruckner.criminalintent.R
+import com.aaronbruckner.criminalintent.fragments.CrimeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
     }
 }
